27375ea8a74b1b58e0972f7d2a201f568384e9be
sammia28 <sammia.zubizarreta@gmail.com>
2016-09-15 15:21:09 -0500

    arreglos contraseña

--- js/main.js
+++ js/main.js
@@ -1,36 +1,46 @@
 function validateForm(){
     var nom = document.getElementById("name").value;
-    if( nom == null || nom.length == 0 || nom == ""){
-        alert("Escribe un nombre.");
-    }else if ( /[0-9]/.test(nom)){
-        alert("No ingrese números.")
-    }else if ( /^[a-z]/.test(nom.charAt(0)) ){
-        alert ("Escriba el primer carácter en mayúscula")
-    }
-
     var apellido = document.getElementById("lastname").value;
-    if( apellido == null || apellido.length == 0 || apellido == ""){
-        alert("Escribe tu apellido.");
-    }else if ( /[0-9]/.test(apellido)){
-        alert("No ingrese números.")
-    }else if ( /^[a-z]/.test(apellido.charAt(0)) ){
-        alert ("Escriba el primer carácter en mayúscula.")
-    }
-
-    var emailRegex=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    cor = document.getElementById("input-email").value;
-    if(!emailRegex.test(cor)) {
-        alert("Ingrese un correo válido.");
-    };
-
+    var cor = document.getElementById("input-email").value;
     var contr = document.getElementById("input-password").value;
-    if (contr.length <= 6 || contr.length == 0 || contr == "123456" || contr == "98754" || contr == "password" ) {
-        alert("Contraseña no válida.");
-    }
-
     var opcion = document.querySelector("select").value;
-    if( opcion == 0 ) {
-        alert("Por favor, seleccione una opción en checkbox.");
-    }
-    return true;    
+
+    if (nom.length == 0 && apellido == 0 && cor == 0 && contr == 0) {
+        alert ("Complete todos campos");
+    } else {
+
+        if( nom == null || nom.length == 0 || nom == ""){
+            alert("Escribe tu nombre.");
+        }else if ( /[0-9]/.test(nom)){
+            alert("No ingrese números.")
+        }else if ( /^[a-z]/.test(nom.charAt(0))){
+            alert ("Escriba el primer caracter de su nombre en mayúscula.")
+        }
+
+        if( apellido == null || apellido.length == 0 || apellido == ""){
+            alert("Escribe tu apellido.");
+        }else if ( /[0-9]/.test(apellido)){
+            alert("No ingrese números.")
+        }else if ( /^[a-z]/.test(apellido.charAt(0)) ){
+            alert ("Escriba el primer caracter de su apellido en mayúscula.")
+        }
+
+        var correos=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!correos.test(cor)) {
+            alert("Ingrese un correo válido.");
+        };
+
+        if (contr == null || contr.length == 0){
+            alert("Ingresa una contraseña");
+        }else if (contr.length < 6){
+            alert("contraseña no valida (debe contener más de 6 caracteres)");
+        }
+        if (contr == "123456" || contr == "098754" || contr == "password"){
+            alert ("123456, 0987654, password; no son contraseñas válidas");
+        }
+
+        if(opcion == 0) {
+            alert("Elige una opción.");
+        }    
+    };
 }
